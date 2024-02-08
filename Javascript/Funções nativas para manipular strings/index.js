54fa7f2e15a7b3037063dbf7437bf96290408fa3
isblackout <dnnxzz70@gmail.com>
2024-02-08 08:48:37 -0300

    new file:   "Javascript/Fun\303\247\303\265es de callback/funcoes_callback.html" 	new file:   "Javascript/Fun\303\247\303\265es nativas para manipular strings/index.html" 	new file:   "Javascript/Fun\303\247\303\265es nativas para manipular strings/index.js"

---
+++ Javascript/Funções nativas para manipular strings/index.js
@@ -0,0 +1,17 @@
+document.write('length'.length + '</br>');
+
+document.write('length'.charAt(1) + '</br>');
+
+document.write('length'.indexOf('g') + '</br>');
+
+document.write('length'.replace('length', 'replace test') + '</br>');
+
+document.write('length'.substring(3, 6) + '</br>');
+
+document.write('length'.toLowerCase() + '</br>');
+
+document.write('length'.toUpperCase() + '</br>');
+
+const nome = '   length    '
+
+document.write(nome.trim() + '</br>');
