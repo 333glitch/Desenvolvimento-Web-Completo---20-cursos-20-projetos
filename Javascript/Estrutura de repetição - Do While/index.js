0dc7ea312926ec9fd981bf4af84e387530a05e80
isblackout <dnnxzz70@gmail.com>
2024-02-15 21:21:12 -0300

    new file:   "Javascript/Estrutura de repeti\303\247\303\243o - Do While/index.html" 	new file:   "Javascript/Estrutura de repeti\303\247\303\243o - Do While/index.js" 	new file:   "Javascript/Estrutura de repeti\303\247\303\243o - While/index.html" 	new file:   "Javascript/Estrutura de repeti\303\247\303\243o - While/index.js"

---
+++ Javascript/Estrutura de repetição - Do While/index.js
@@ -0,0 +1,8 @@
+var x = 0
+var y = 10
+
+do {
+    document.write(x, '</br>')
+    x++
+} while (x <= 10);
+
---
+++ Javascript/Estrutura de repetição - While/index.js
@@ -0,0 +1,8 @@
+var x = 0
+var y = 10
+
+while (x <= 10) {
+    document.write(x, '</br>')
+    x++
+}
+
